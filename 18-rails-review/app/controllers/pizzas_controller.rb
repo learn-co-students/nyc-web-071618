927b3c116c7a19cc1878bd3bb9098cec6a132fda
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-14 11:06:54 -0400

    RAIL REVIEW PART 2

--- 18-rails-review/app/controllers/pizzas_controller.rb
+++ 18-rails-review/app/controllers/pizzas_controller.rb
@@ -1,4 +1,5 @@
 class PizzasController < ApplicationController
+  before_action(:find_pizza, { only: [:show, :edit, :update, :destroy] })
 
   def index # GET /pizzas
     @pizzas = Pizza.all
@@ -6,7 +7,7 @@ class PizzasController < ApplicationController
   end
 
   def show # GET /pizzas/:id
-    @pizza = Pizza.find(params[:id])
+    # @pizza = Pizza.find(params[:id])
     render :show
   end
 
@@ -16,7 +17,7 @@ class PizzasController < ApplicationController
   end
 
   def create # POST /pizzas -> new form will post here
-    @pizza = Pizza.create(pizza_params)
+    @pizza = Pizza.create(pizza_params) #-> invokes pizza_params when I reference it
     if @pizza.valid?
       # flash will sends a piece of data via sessions/cookies that expires after one HTTP request
       flash[:notice] = 'Successfully created pizza!'
@@ -28,16 +29,36 @@ class PizzasController < ApplicationController
   end
 
   def edit # GET /pizzas/:id/edit
-    render :new
+    # @pizza = Pizza.find(params[:id]) #=> find the pizza the user wants to edit based on id found in route params -> /pizzas/4/edit
+    render :edit
   end
 
   def update # PATCH /pizzas/:id
+    if @pizza.update(pizza_params)
+      flash[:notice] = 'Successfully updated pizza'
+      # redirect_to pizza_path(@pizza)
+      redirect_to(@pizza)
+    else
+      render :edit
+    end
+    # @pizza = Pizza.find(params[:id]) #=> find the pizza the user wants to edit based on id found in route params -> /pizzas/4
     #code
+  end
+
+  def destroy
+    @pizza.destroy
+    flash[:notice] = 'Successfully deleted pizza'
+    redirect_to pizzas_path
   end
 
   private
   def pizza_params
+    # {authenticity_token: 'askdjf;laksdjflksdjlkjf', pizza: {toppings: 'pepperoni', tasty: true}}
     params.require(:pizza).permit(:toppings, :tasty, :price, :size)
+  end
+
+  def find_pizza
+    @pizza = Pizza.find(params[:id])
   end
 
 end
--- 18-rails-review/app/models/pizza.rb
+++ 18-rails-review/app/models/pizza.rb
@@ -1,2 +1,12 @@
 class Pizza < ApplicationRecord
+  validates :toppings, presence: :true
+  validates :price, numericality: { greater_than_or_equal_to: 1 }
+  # custom validations
+  validate :size_must_be_large
+
+  def size_must_be_large
+    unless self.size == 'large'
+      self.errors.add(:size, 'MUST BE LARGE')
+    end
+  end
 end
